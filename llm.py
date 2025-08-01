b011e045a6dd9d87a41b91ce014a48d96aef2df4
esonia42 <soniarojkova42@gmail.com>
2025-08-01 15:29:44 -0300

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,10 +1,11 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyCs_HydhUScy2pMK25JVHQ1pepNtGkygGo")
+client = genai.Client(api_key="")
 
 response = client.models.generate_content(
     model="gemini-2.5-pro",
     contents="Why is the sky blue?"
 )
+
 
 print(response.text)
